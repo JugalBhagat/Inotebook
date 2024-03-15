b9516652dfa52cfec6d1bc609b5e327b3d912ee4
jugal <jugalbhagat017@gmail.com>
2024-03-15 22:25:28 +0530

    Tutorial-44 done

---
+++ backend/models/Notes.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const NotesSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    tags:{
+        type: String,
+        default:"Unknown"
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+});
+
+module.exports=mongoose.model('notes',NotesSchema);
