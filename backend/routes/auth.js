827a73ee6cbd35cba6963aa2442c0b16c0d2c36c
jugal <jugalbhagat017@gmail.com>
2024-03-19 11:48:55 +0530

    Tutorial-50 done (Login Endpoint created)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,8 +4,9 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
+const JWT_SECRET="jUgALBhaGAt";
 
-//create user using POST "/api/auth/"
+//create user using POST "/api/auth/createuser"
 router.post('/createuser', [
     // Adding Validations
     body('name', "Enter Name").notEmpty(),
@@ -43,7 +44,6 @@ router.post('/createuser', [
                 id:user.id
             }
         }
-        const JWT_SECRET="jUgALBhaGAt"
         const authToken=jwt.sign(data,JWT_SECRET);
         console.log(authToken)
 
@@ -53,6 +53,44 @@ router.post('/createuser', [
     }catch(error){
         console.error(error.message);
         res.status(500).send("Something went wrong");   
+    }
+});
+
+//create user using POST "/api/auth/login"
+router.post('/login', [
+    // Adding Validations
+    body('email', "Enter Valid Email").isEmail(),
+    body('password', "Password must be 6 digit long").isLength({ min: 6 }),
+], async (req, res) => {
+    const errors = validationResult(req);
+    // check if any errors
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: errors.array() });
+    }
+
+    const {email,password}=req.body;
+    try{
+        let user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:"Incorrect Password"});
+        }
+        const passCompare=await bcrypt.compare(password,user.password);
+        if(!passCompare){
+            return res.status(400).json({error:"Incorrect Password"});
+        }
+        const payload={
+            user:{
+                id:user.id
+            }
+        }
+        const authToken=jwt.sign(payload,JWT_SECRET);
+        // console.log("AuthToken = ",authToken)
+        res.json({"authToken":authToken})
+
+    }catch(error)
+    {
+        // console.error(error.message);
+        res.status(500).send("Something went Wrong");   
     }
 });
 
