50530415c6debf9e58765f6e72990558e819e92f
jugal <jugalbhagat017@gmail.com>
2024-03-22 08:21:51 +0530

    Tutorial-54.5 Adding permission for calling backend from port-no:3000 in index.js file (Making a sign up page on my Own)

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,12 @@ const port = 4000
 app.use(express.json())
 
 //Available Routes
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/note',require('./routes/note'))
 
